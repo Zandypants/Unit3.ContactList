7ea39ee14a64d29ee7f7c211baa0aa19a4de078a
Alexander Linse <zandervon24@gmail.com>
2024-03-27 12:04:32 -0500

    Added list of contact names

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
+
+const API_URL = `https://jsonplaceholder.typicode.com/`;
+
+async function fetchUsers(setUserList) {
+  const response = await fetch(API_URL + "users");
+  const json = await response.json();
+  console.log("fetchedJson:", json);
+
+  setUserList(json);
+}
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  // On Page Load
+  useEffect(() => {fetchUsers(setContacts)}, []);
 
   return (
     <>
       <h1>Add Contact List here!</h1>
+      <ul>{
+          contacts.map(contact => <li key={contact.id}>{contact.name}</li>)
+      }</ul>
     </>
   )
 }
