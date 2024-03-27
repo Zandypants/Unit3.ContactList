46bcbafcf20307a5460014c6a4327139c5bd191b
Alexander Linse <zandervon24@gmail.com>
2024-03-27 14:12:06 -0500

    Added try/catch to fetchUsers

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ import './App.css'
 const API_URL = `https://jsonplaceholder.typicode.com/`;
 
 async function fetchUsers(setUserList) {
-  const response = await fetch(API_URL + "users");
-  const json = await response.json();
+  try {
+    const response = await fetch(API_URL + "users");
+    const json = await response.json();
 
-  setUserList(json);
+    setUserList(json);
+  } catch (error) {
+    console.log(`Error caught: ${error}`);
+  }
 }
 
 function App() {
+  // State
   const [contacts, setContacts] = useState([]);
 
   // On Page Load
   useEffect(() => {fetchUsers(setContacts)}, []);
 
+  // Render
   return (
     <>
       <h1>Contact List</h1>
